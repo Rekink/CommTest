722053a76bab9c022fa755ce40c8bff46cd41bc6
zhoukezheng <zhoukz@rocksea.com.cn>
2019-08-02 15:32:57 +0800

    update first

---
+++ MainForm.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace CommTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Label label5;
+            System.Windows.Forms.Label label6;
+            System.Windows.Forms.Label label8;
+            System.Windows.Forms.Label label7;
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.textBoxSendData = new System.Windows.Forms.TextBox();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.comboBoxBaudrate = new System.Windows.Forms.ComboBox();
+            this.labelSendCount = new System.Windows.Forms.Label();
+            this.labelReceiveCount = new System.Windows.Forms.Label();
+            this.textBoxTimeout = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttontimer = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(84, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(44, 17);
+            label1.TabIndex = 5;
+            label1.Text = "串口：";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(12, 46);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(116, 17);
+            label2.TabIndex = 6;
+            label2.Text = "数据（十六进制）：";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(60, 84);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(68, 17);
+            label3.TabIndex = 7;
+            label3.Text = "文件位置：";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(60, 123);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(68, 17);
+            label4.TabIndex = 8;
+            label4.Text = "文件名称：";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(279, 9);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(56, 17);
+            label5.TabIndex = 12;
+            label5.Text = "波特率：";
+            // 
+            // label6
+            // 
+            label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(430, 185);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(44, 17);
+            label6.TabIndex = 8;
+            label6.Text = "发送：";
+            // 
+            // label8
+            // 
+            label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            label8.AutoSize = true;
+            label8.Location = new System.Drawing.Point(512, 185);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(44, 17);
+            label8.TabIndex = 8;
+            label8.Text = "接收：";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(43, 162);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(85, 17);
+            label7.TabIndex = 8;
+            label7.Text = "超时（ms）：";
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(134, 6);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(79, 25);
+            this.comboBoxPort.TabIndex = 1;
+            // 
+            // textBoxSendData
+            // 
+            this.textBoxSendData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSendData.Location = new System.Drawing.Point(134, 43);
+            this.textBoxSendData.Name = "textBoxSendData";
+            this.textBoxSendData.Size = new System.Drawing.Size(478, 23);
+            this.textBoxSendData.TabIndex = 2;
+            this.textBoxSendData.Text = "01 04 00 00 00 02 71 CB";
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFolder.Location = new System.Drawing.Point(134, 81);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.ReadOnly = true;
+            this.textBoxFolder.Size = new System.Drawing.Size(399, 23);
+            this.textBoxFolder.TabIndex = 10;
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(134, 120);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(130, 23);
+            this.textBoxFileName.TabIndex = 4;
+            this.textBoxFileName.Text = "TestLog";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.Location = new System.Drawing.Point(539, 76);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(73, 33);
+            this.buttonBrowse.TabIndex = 3;
+            this.buttonBrowse.Text = "浏览";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLog.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.textBoxLog.Location = new System.Drawing.Point(12, 241);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(600, 117);
+            this.textBoxLog.TabIndex = 9;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Font = new System.Drawing.Font("微软雅黑", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonSend.Location = new System.Drawing.Point(63, 380);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(217, 49);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "发送";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // comboBoxBaudrate
+            // 
+            this.comboBoxBaudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBaudrate.FormattingEnabled = true;
+            this.comboBoxBaudrate.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaudrate.Location = new System.Drawing.Point(341, 6);
+            this.comboBoxBaudrate.Name = "comboBoxBaudrate";
+            this.comboBoxBaudrate.Size = new System.Drawing.Size(79, 25);
+            this.comboBoxBaudrate.TabIndex = 11;
+            // 
+            // labelSendCount
+            // 
+            this.labelSendCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelSendCount.AutoSize = true;
+            this.labelSendCount.Location = new System.Drawing.Point(480, 185);
+            this.labelSendCount.Name = "labelSendCount";
+            this.labelSendCount.Size = new System.Drawing.Size(15, 17);
+            this.labelSendCount.TabIndex = 8;
+            this.labelSendCount.Text = "0";
+            // 
+            // labelReceiveCount
+            // 
+            this.labelReceiveCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelReceiveCount.AutoSize = true;
+            this.labelReceiveCount.Location = new System.Drawing.Point(562, 185);
+            this.labelReceiveCount.Name = "labelReceiveCount";
+            this.labelReceiveCount.Size = new System.Drawing.Size(15, 17);
+            this.labelReceiveCount.TabIndex = 8;
+            this.labelReceiveCount.Text = "0";
+            // 
+            // textBoxTimeout
+            // 
+            this.textBoxTimeout.Location = new System.Drawing.Point(134, 159);
+            this.textBoxTimeout.Name = "textBoxTimeout";
+            this.textBoxTimeout.Size = new System.Drawing.Size(130, 23);
+            this.textBoxTimeout.TabIndex = 4;
+            this.textBoxTimeout.Text = "200";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttontimer
+            // 
+            this.buttontimer.Font = new System.Drawing.Font("微软雅黑", 16F);
+            this.buttontimer.Location = new System.Drawing.Point(305, 380);
+            this.buttontimer.Name = "buttontimer";
+            this.buttontimer.Size = new System.Drawing.Size(217, 49);
+            this.buttontimer.TabIndex = 16;
+            this.buttontimer.Text = "定时发送";
+            this.buttontimer.UseVisualStyleBackColor = true;
+            this.buttontimer.Click += new System.EventHandler(this.buttontimer_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 202);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "位移值（mm）:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("微软雅黑", 20F);
+            this.label10.Location = new System.Drawing.Point(138, 194);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 35);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "0.000";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.buttontimer);
+            this.Controls.Add(this.comboBoxBaudrate);
+            this.Controls.Add(label5);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxTimeout);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.textBoxSendData);
+            this.Controls.Add(this.labelReceiveCount);
+            this.Controls.Add(label8);
+            this.Controls.Add(this.labelSendCount);
+            this.Controls.Add(label6);
+            this.Controls.Add(label7);
+            this.Controls.Add(label4);
+            this.Controls.Add(label3);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "MainForm";
+            this.Text = "串口测试";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.TextBox textBoxSendData;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ComboBox comboBoxBaudrate;
+        private System.Windows.Forms.Label labelSendCount;
+        private System.Windows.Forms.Label labelReceiveCount;
+        private System.Windows.Forms.TextBox textBoxTimeout;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttontimer;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CommTest
+{
+    public partial class MainForm : Form
+    {
+        private readonly byte[] _receiveBuffer = new byte[2048];
+        private int _receiveCount;
+        private int _displayCount;
+
+        public MainForm()
+        {           
+            InitializeComponent();
+
+            // 枚举串口
+            comboBoxPort.Items.AddRange(SerialPort.GetPortNames());
+            if (comboBoxPort.Items.Count > 0)
+                comboBoxPort.SelectedIndex = 0;
+
+            comboBoxBaudrate.SelectedIndex = 1;
+
+            // 设置默认存盘路径为桌面
+            textBoxFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\";
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            var dlg = new FolderBrowserDialog();
+            dlg.Description = "请选择存盘文件夹：";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            var folder = dlg.SelectedPath.Trim();
+            if (string.IsNullOrEmpty(folder))
+                return;
+
+            if (!folder.EndsWith("\\"))
+                folder += "\\";
+            textBoxFolder.Text = folder;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {            
+                if (string.IsNullOrEmpty(comboBoxPort.Text))
+                    throw new Exception("没有可用的串口！");
+                
+                // 读取要发送的数据
+                var sendData = ReadSendData();
+
+                // 验证输出文件是否能成功创建
+                if (string.IsNullOrEmpty(textBoxFileName.Text.Trim()))
+                    throw new Exception("输出文件名不能为空！");
+                var outputFileName = textBoxFolder.Text + textBoxFileName.Text.Trim() + ".txt";
+                using (var stream = new FileStream(outputFileName, FileMode.Append))
+                {
+                }
+
+                // 校验超时值的范围
+                int timeout = 0;
+                int.TryParse(textBoxTimeout.Text, out timeout);
+                if ((timeout < 50) || (timeout > 1000))
+                    throw new Exception("超时时间必须在 50ms ~ 1000ms 之间！");
+
+                // 设置读取超时值为 200ms，并打开串口
+                var serialPort = new SerialPort(comboBoxPort.Text, int.Parse(comboBoxBaudrate.Text));
+                serialPort.ReadTimeout = timeout;
+                serialPort.Open();
+
+                // 发送数据
+                serialPort.Write(sendData, 0, sendData.Length);
+
+                // 记录发送日志
+                using (var stream = new FileStream(outputFileName, FileMode.Append))
+                {
+                    using (var writer = new StreamWriter(stream, Encoding.Default))
+                    {
+                        writer.Write(getSendLog(sendData));
+                    }
+                }
+
+                // 更新显示
+                labelSendCount.Text = sendData.Length.ToString();
+                var sb = new StringBuilder("\r\nSend :");
+                foreach (var b in sendData)
+                    sb.AppendFormat(string.Format(" {0:X2}", b));              
+                sb.Append("\r\n\r\nReceive :");
+                textBoxLog.Text = sb.ToString();
+                
+                //buttonSend.Enabled = false;
+
+                // 读取数据
+                var bw = new BackgroundWorker();
+                bw.WorkerReportsProgress = true;
+                bw.ProgressChanged += Bw_ProgressChanged;
+                bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+                bw.DoWork += Bw_DoWork;
+                bw.RunWorkerAsync(serialPort);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            _receiveCount = 0;
+            _displayCount = 0;
+
+            var bw = sender as BackgroundWorker;
+            var serialPort = e.Argument as SerialPort;
+            while (true)
+            {
+                try
+                {
+                    // 每次尝试读取 16 个字节
+                    var readCount = serialPort.Read(_receiveBuffer, _receiveCount, Math.Min(9, _receiveBuffer.Length - _receiveCount));
+                    _receiveCount += readCount;
+
+                    // 本次读取结束
+                    if (_receiveCount >= 9)
+                        break;
+                }
+                catch (TimeoutException)
+                {
+                    // 发生超时，本次读取结束
+                    break;
+                }
+            }
+
+            bw.ReportProgress(_receiveCount);
+            serialPort.Close();
+        }
+   
+        private void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {       
+            var sb = new StringBuilder();
+            int end = e.ProgressPercentage;
+            for (int i = _displayCount; i < end; i++)
+                sb.AppendFormat(" {0:X2}", _receiveBuffer[i]);
+            _displayCount = end;
+
+            //var sb = new StringBuilder(" 01 04 04 00 20 7C 4E 5A BA"); 
+
+            // 显示接收到的数据
+            labelReceiveCount.Text = end.ToString();
+            textBoxLog.Text += sb.ToString();         
+            textBoxLog.SelectionStart = textBoxLog.Text.Length;
+            textBoxLog.ScrollToCaret();
+           
+        }
+
+        private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            try
+            {
+                if (_receiveCount == 9)
+                {                 
+                    float value = _receiveBuffer[3] * 256 + _receiveBuffer[4];
+                    value += (_receiveBuffer[5] * 256 + _receiveBuffer[6]) / 65535f;
+                    label10.Text= string.Format("{0:F3}\r\n", value);                   
+                }
+
+                //buttonSend.Enabled = true;
+
+                // 记录接收日志
+                var outputFileName = textBoxFolder.Text + textBoxFileName.Text.Trim() + ".txt";
+                using (var stream = new FileStream(outputFileName, FileMode.Append))
+                {
+                    using (var writer = new StreamWriter(stream, Encoding.Default))
+                        writer.Write(GetReceiveLog(_receiveBuffer, _receiveCount));
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+
+        /// <summary>
+        /// 读取要发送的数据
+        /// </summary>
+        /// <returns>由发送的字符串转换后的 byte[]</returns>
+        private byte[] ReadSendData()
+        {
+            string[] stringArray = textBoxSendData.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (stringArray.Length == 0)
+                throw new Exception("没有可用的发送数据！");
+
+            var dataArray = new byte[stringArray.Length];
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                int value = Convert.ToInt32(stringArray[i], 16);
+                if ((value < 0) || (value > 255))
+                    throw new Exception("发送数据无法转换为有效的十六进制数据！");
+
+                dataArray[i] = (byte)value;
+            }
+
+            return dataArray;
+        }
+
+        /// <summary>
+        /// 生成发送日志
+        /// </summary>
+        /// <param name="sendData"></param>
+        /// <returns></returns>
+        private static string getSendLog(byte[] sendData)
+        {
+            // 记录时间、发送字节数
+            var sb = new StringBuilder();
+            sb.AppendFormat("({0})_Send({1}):", DateTime.Now.ToString("HH:mm:ss:fff"), sendData.Length);
+
+            // 记录发送数据
+            foreach (var b in sendData)
+                sb.AppendFormat(string.Format(" {0:X2}", b));
+
+            // 换行
+            sb.Append("\r\n");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 生成接收日志
+        /// </summary>
+        /// <param name="receiveData"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private static string GetReceiveLog(byte[] receiveData, int count)
+        {
+            var sb = new StringBuilder();
+
+            // 记录时间、接收字节数
+            sb.AppendFormat("({0})_Receive({1}):", DateTime.Now.ToString("HH:mm:ss:fff"), count);
+
+            // 记录接收数据
+            for (int i = 0; i < count; i++)
+                sb.AppendFormat(" {0:X2}", receiveData[i]);
+
+            // 换行
+            sb.Append("\r\n\r\n");
+
+            return sb.ToString();
+        }
+
+        private void buttontimer_Click(object sender, EventArgs e)
+        {                             
+            timer1.Enabled = true;
+            timer1.Interval = 500;          
+        }
+       
+    }
+}
